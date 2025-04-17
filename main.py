850fc2d8b270b441b3a66d06b946a508f81b10f6
Jack Harper <jack.harper@stfc.ac.uk>
2025-04-17 20:51:41 +0100

    working

--- main.py
+++ main.py
@@ -16,20 +16,26 @@ input("please log in, then navigate to the payslips page, then hit enter")
 payslips_dropdown = driver.find_element(by=By.ID, value="AdvicePicker")
 payslips = Select(payslips_dropdown)
 
-option_list = payslips.options
-
-print(option_list)
+option_list = [option.text for option in payslips.options]
 
 for i, option in enumerate(option_list):
+    print(f"doing option {i}, payslips: {option}")
     payslips_dropdown = driver.find_element(by=By.ID, value="AdvicePicker")
     payslips = Select(payslips_dropdown)
 
     payslips.select_by_index(i)
     # find "go" button and click it
-    #wait 15 seconds (hopefully this is long enough for it to give us a pdf)
     go_button = driver.find_element(by=By.ID, value="Go")
     go_button.click()
-    sleep(20)
+    #wait 15 seconds (hopefully this is long enough for it to give us a pdf)
+    sleep(10)
+    go_button = driver.find_element(by=By.ID, value="Go")
+    go_button.click()
+    sleep(10)
+    export_button = driver.find_element(by=By.ID, value="Export")
+    export_button.click()
+
+
 
 
 
